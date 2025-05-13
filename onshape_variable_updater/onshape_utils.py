1ac7e36cb13815b26f1095d5722b3f28f2dc69f7
Kent Kallsen <kentkallsen@gmail.com>
2025-05-13 10:02:07 -0500

    Fix HMAC header formatting for Onshape API

--- onshape_variable_updater/onshape_utils.py
+++ onshape_variable_updater/onshape_utils.py
@@ -10,12 +10,16 @@ BASE_URL = "https://cad.onshape.com"
 
 
 def create_headers(method, url_path):
-    date = time.strftime("%a, %d %b %Y %H:%M:%S GMT", time.gmtime())
-    hmac_string = f'(request-target): {method.lower()} {url_path}\ndate: {date}'
-    signature = base64.b64encode(hmac.new(SECRET_KEY.encode('utf-8'), hmac_string.encode('utf-8'), hashlib.sha256).digest()).decode('utf-8')
+    date = time.strftime('%a, %d %b %Y %H:%M:%S GMT', time.gmtime())
+    hmac_data = f'(request-target): {method.lower()} {url_path}\ndate: {date}'
+    signature = base64.b64encode(
+        hmac.new(SECRET_KEY.encode('utf-8'), hmac_data.encode('utf-8'), digestmod=hashlib.sha256).digest()
+    ).decode('utf-8')
+
     headers = {
         'Authorization': f'On {ACCESS_KEY}:HmacSHA256:{signature}',
         'Date': date,
         'Content-Type': 'application/json'
     }
     return headers
+
